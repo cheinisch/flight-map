57a35ec076d83b98d09077e5e35a01853537d93d
Christian Heinisch <chris@christian-heinisch.de>
2025-01-05 13:14:09 +0100

    Update flight-map.js

--- static/js/flight-map.js
+++ static/js/flight-map.js
@@ -195,6 +195,7 @@ async function initMap() {
     map = L.map('map').setView([config.position.lat, config.position.lon], 10);
     //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', { maxZoom: 19 }).addTo(map);
+    await fetchSquawkColors(); // Farben laden
     updatePlanesOnMap();
 }
 
